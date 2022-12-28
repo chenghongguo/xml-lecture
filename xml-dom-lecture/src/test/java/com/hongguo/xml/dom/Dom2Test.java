62f73a2221bb6d911624cf9d1d6af74dec7950b9
chenghongguo <hongguo_cheng@163.com>
2022-12-28 23:22:39 +0800

    fix: auto import

--- xml-dom-lecture/src/test/java/com/hongguo/xml/dom/Dom2Test.java
+++ xml-dom-lecture/src/test/java/com/hongguo/xml/dom/Dom2Test.java
@@ -1,7 +1,10 @@
 package com.hongguo.xml.dom;
 
 import org.junit.Test;
-import org.w3c.dom.*;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
